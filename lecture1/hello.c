791eba8307dcb01304bb6067505bc3e5b59ad972
isurudotxyz <agisuru@icloud.com>
2023-09-11 12:14:48 +0200

    till loops and variables

---
+++ lecture1/hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <cs50h.h>
+
+int main(void){
+  string answer = get_string("What's your name? ")
+  printf("hello, %s\n", answer)
+}
+// make  in terminal to compile file 
+
+
+
